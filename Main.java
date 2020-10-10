accbf40325a2c568682c22aac5fd6c87d2c50dbb
qurek9038 <71150179+qurek9038@users.noreply.github.com>
2020-10-10 01:03:34 -0400

    Update Main.java
    Question A4Q1 Completed

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 /**
- *
+ *This program will start at 100 and count down by 5 until it gets as low as possible without going pased the users entered number
  * @author Khazina
  */
 public class Main {
@@ -9,12 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    //Scanner
+    //Create the scanner for user input
+    Scanner input = new Scanner(System.in);
 
-    //Display 
+    //Ask the user for an integer to count down to 
     System.out.println("Please enter an integer to count down to");
 
+    //Store user input using variable
+    int down = input.nextInt();
 
+    //Pre loop code 
+    System.out.println("Count Down:");
+
+    //Create a counter variable
+    int count = 100;
+
+    //Use a loop to count down from hundred by fives without going passed users integer
+    while(count >= down){
+      //Print the number we are on
+      System.out.println(count);
+
+      //Add five to count/ accumulator
+      count = count - 5;
+    }
   }
 }
+
